82ec1ad797bf908276cb8f3480b3dd36fcb3b4d8
sasipreethamgonapa <132178865+sasipreethamgonapa@users.noreply.github.com>
2023-09-25 22:50:51 +0530

    Create SuccessScreen.js

---
+++ SuccessScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SuccessScreen() {
+  return (
+    <div>
+      <h2>Success - Screen 3</h2>
+      <p>OTP verification successful!</p>
+    </div>
+  );
+}
+
+export default SuccessScreen;
